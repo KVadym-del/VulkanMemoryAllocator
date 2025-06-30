7795709c82bf11985e87130921b94283ce6a02ef
Vadym Kozachuk <77024298+KVadym-del@users.noreply.github.com>
2025-06-30 21:43:30 +0200

    Fixed premake5 file.

--- premake5.lua
+++ premake5.lua
@@ -1,55 +1,14 @@
-workspace "VulkanMemoryAllocator"
-    architecture "x64"
-    startproject "VulkanMemoryAllocator"
-
-    configurations
-    {
-        "Debug",
-        "Release"
-    }
-
-    location "build"
-
-outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-
 project "VulkanMemoryAllocator"
     kind "StaticLib"
     language "C++"
-    cppdialect "C++14"
-    staticruntime "On"
+    cppdialect "C++17"
 
-    targetdir ("lib/" .. outputdir)
-    objdir ("obj/" .. outputdir)
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files
-    {
-        "include/vk_mem_alloc.h",
-        "src/vma_lib.cpp"
+    includedirs { "%{prj.location}/include" }
+
+    files {
+        "%{prj.location}/include/vk_mem_alloc.h",
+        "%{prj.location}/src/vma_lib.cpp"
     }
-
-    includedirs
-    {
-        "include",
-        "%{VULKAN_SDK}/Include"
-    }
-
-    vpaths
-    {
-        ["Headers"] = { "include/**.h" },
-        ["Sources"] = { "src/**.cpp" }
-    }
-
-    filter "configurations:Debug"
-        defines { "VMA_DEBUG_LOG_FORMAT(format, ...)" }
-        runtime "Debug"
-        symbols "On"
-
-    filter "configurations:Release"
-        runtime "Release"
-        optimize "On"
-
-    filter "system:windows"
-        systemversion "latest"
-
-    filter "system:linux"
-        pic "On"
